f5493e262d9f2400cf82baf7785beccf95df0704
bytecrow <bytecrow@elixired.com>
2024-10-01 23:59:31 +0300

    add streaming autostart

--- config/config.exs
+++ config/config.exs
@@ -9,6 +9,12 @@
 # move said applications out of the umbrella.
 import Config
 
+config :streamer, Streamer.Repo,
+  database: "streamer_repo",
+  username: "user",
+  password: "pass",
+  hostname: "localhost"
+
 config :naive,
   ecto_repos: [Naive.Repo],
   binance_client: BinanceMock,
@@ -24,6 +30,15 @@ config :naive,
 
 config :naive, Naive.Repo,
   database: "naive",
+  username: "postgres",
+  password: "hedgehogSecretPassword",
+  hostname: "localhost"
+
+config :streamer,
+  ecto_repos: [Streamer.Repo]
+
+config :streamer, Streamer.Repo,
+  database: "streamer",
   username: "postgres",
   password: "hedgehogSecretPassword",
   hostname: "localhost"
